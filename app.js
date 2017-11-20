cb276e26ce35facf26401f3c8dd00c2442eb6686
psasa01 <psasa01@gmail.com>
2017-11-20 16:22:59 +0100

    201117

--- app.js
+++ app.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose/');
 
 const routes = require('./routes/routes')
 
 const app = express();
+
+mongoose.Promise = global.Promise;
+var db = mongoose.createConnection('mongodb://localhost:27017/muber', {
+  useMongoClient: true
+});
 
 app.use(bodyParser.json());
 routes(app);
