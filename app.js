a1188f225915c694cc33aaef23995eb37ebea9f3
psasa01 <psasa01@gmail.com>
2017-11-21 14:10:27 +0100

    211117

--- app.js
+++ app.js
@@ -7,15 +7,18 @@ const routes = require('./routes/routes')
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/muber', {
-   useMongoClient: true
-}, (err) => {
-   if (err) {
+
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost:27017/muber', {
+    useMongoClient: true
+  }, (err) => {
+    if (err) {
       console.log('error!!!' + err);
-   } else {
+    } else {
       console.log('connected to DATAbaSEEEEE!!!!!!');
-   }
-});
+    }
+  });
+}
 
 app.use(bodyParser.json());
 routes(app);
---
+++ test/test_helper.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+before((done) => {
+  mongoose.connect('mongodb://localhost:27017/muber_test', {
+    useMongoClient: true
+  });
+  mongoose.connection
+    .once('open', () => {
+      console.log('connectttedeedd to TES_TTTTS_STT')
+      done();
+    })
+    .on('error', err => {
+      console.warn('Warning', error);
+    });
+});
